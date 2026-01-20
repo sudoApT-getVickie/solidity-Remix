266b2c0dbf0162daa4d79be6a8d466b61401e65f
sudoApT-getVickie <123264661+sudoApT-getVickie@users.noreply.github.com>
2026-01-20 20:21:51 +0300

    MERITPOINT PROJECT

---
+++ MERITPOINT.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MERITPOINT {
+        
+        string  public studentName;//for the name of the student example name = "David"
+        address public Instructoraddress; //for the Instructor as an address      
+        uint public meritPoints = 0;
+        
+        constructor(string memory _name) {
+            studentName = _name;
+            Instructoraddress = msg.sender;
+        }
+
+        function addPoint(uint _points) public {
+            require(msg.sender == Instructoraddress,"Only Instructor Should add MeritPoints");
+            meritPoints += _points; //my function to add merit points
+        }
+        function removePoint(uint _points) public {
+             require(msg.sender == Instructoraddress,"Only Instructor Should remove MeritPoints");
+           
+             require(meritPoints >=_points,"Student doesn't have enough Merit Points");
+              // the require statemnt that ensures that the student's merit points never drop below zero 
+               meritPoints -= _points; //my function to reduce merit points
+    }
+        
+        }
+        
